4dc72105e0d6d82b5e5d6e783d04937fb11c9771
phiroun <man.phiroun@gmail.com>
2023-09-26 20:24:00 -0700

    router working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
 import Products from './components/Products.jsx'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
-
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={Products} />
-      </Switch>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
     </>
   )
 }
-
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,13 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-sm">
                 <div className="container">
-                    <NavLink className="navbar-brand fw-bold fs-4" to="#">Apsara eShop</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <NavLink className="navbar-brand fw-bold fs-4" to="/">Apsara eShop</NavLink>
+                    <button className="navbar-toggler" type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -17,28 +22,28 @@ function Navbar() {
                         </form>
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="#">Home</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="#">Products</NavLink>
+                                <NavLink className="nav-link" to="/products">Products</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link register" aria-disabled="true">About</NavLink>
+                                <NavLink className="nav-link" to="/about">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link cart" aria-disabled="true">Contact</NavLink>
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
                             </li>
                         </ul>
                         <div className="buttons">
-                            <NavLink to="" className='btn btn-outline-dark'>
+                            <NavLink to="/login" className='btn btn-outline-dark'>
                                 <i className="fa fa-sign-in me-1"></i>
                                 <span className="hide-sm">Login</span>
                             </NavLink>
-                            <NavLink to="" className='btn btn-outline-dark ms-2'>
+                            <NavLink to="/register" className='btn btn-outline-dark ms-2'>
                                 <i className="fa fa-user-plus me-1"></i>
                                 <span className="hide-sm">Register</span>
                             </NavLink>
-                            <NavLink to="" className='btn btn-outline-dark ms-2'>
+                            <NavLink to="/cart" className='btn btn-outline-dark ms-2'>
                                 <i className="fa fa-shopping-cart me-1"></i>
                                 <span className="hide-sm">Cart</span>
                             </NavLink>
